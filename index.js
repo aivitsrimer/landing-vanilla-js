12208317e930dfbeaecac9ae8a93096b8693042b
Anna Vits-Rimer <vitsrimer.anna@gmail.com>
2021-09-22 20:43:15 +0700

    Added js for showing files to upload. Fixed styles for files

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ let genderSelect = document.querySelector('#gender');
 let countryInput = document.querySelector('#country');
 let cityInput = document.querySelector('#city');
 let dobInput = document.querySelector('#dob');
+let filesInput = document.querySelector('#files');
 
 let addFileButton = document.querySelector('.uploader .add');
 let sendInfoButton = document.querySelector('.send [type="submit"]');
+
+let fileList = [];
 
 nameInput.addEventListener('change', fieldsHandler);
 genderSelect.addEventListener('change', fieldsHandler);
@@ -13,7 +16,8 @@ countryInput.addEventListener('change', fieldsHandler);
 cityInput.addEventListener('change', fieldsHandler);
 dobInput.addEventListener('change', fieldsHandler);
 
-addFileButton.addEventListener('click', addFileHandler);
+addFileButton.addEventListener('click', () => filesInput.click());
+filesInput.addEventListener('change', fileChangeHandler);
 
 function showNextStep(nextStepId) {
     if (nextStepId <= 3) {
@@ -34,6 +38,38 @@ function fieldsHandler(event) {
     }
 }
 
-function addFileHandler() {
-    document.getElementById('file').click();
+function fileChangeHandler() {
+    for (let i = 0; i < filesInput.files.length; i++) {
+        fileList.push(filesInput.files[i]);
+    }
+    fileList.forEach((file) => {
+        let nameSplitted = file.name.split('.');
+        let name = nameSplitted[0];
+        let size = calculateHumanFileSize(file.size);
+        let extension = nameSplitted[nameSplitted.length - 1].toUpperCase();
+        console.log(file);
+        console.log(name, size, extension);
+
+        let template = `<div class="file">
+            <div class="file-info">
+              <div class="file-data">
+                <p class="file-name">${name}</p>
+                <p class="file-size">${extension} ${size}</p>
+              </div>
+            </div>
+            <a href="#" class="file-delete"><img src="./assets/trashbin.svg" alt="delete"></a>
+          </div>`;
+        document.querySelector('.files').innerHTML = template;
+    })
+}
+
+function calculateHumanFileSize(fileSizeInBytes) {
+    let i = -1;
+    let byteUnits = [' kb', ' mb'];
+    do {
+        fileSizeInBytes = fileSizeInBytes / 1024;
+        i++;
+    } while (fileSizeInBytes > 1024 && i < byteUnits.length - 1);
+
+    return Math.max(fileSizeInBytes, 0.1).toFixed(1) + byteUnits[i];
 }
